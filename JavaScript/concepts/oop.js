175fe4ae5ad6b00223e97f97527e283fbdadac12
mgoscar <mgoscar@gmail.com>
2022-06-07 21:43:31 -0500

    OOP concepts update

--- JavaScript/concepts/oop.js
+++ JavaScript/concepts/oop.js
@@ -76,3 +76,25 @@ function imprimir(tipo) { //Polimorfismo,
 
 imprimir(gerente1);
 imprimir(empleado1);
+
+
+/*
+ =========   basic principles of OOP  =========
+ 1) Encapsulation.- Encapsulation is achieved when each object keeps its state private, inside a class. 
+    Other objects don’t have direct access to this state. Instead, they can only call a list of public functions.
+ 2) Abstraction.- This mechanism should hide internal implementation details. It should only reveal operations 
+    relevant for the other objects. Abstraction can be thought of as a natural extension of encapsulation.
+ 3) Inheritance (Herencia) .- It is the ability to acquire the properties of existing classes and 
+    create new ones. Inheritance allows you to reuse code without having to rewrite it in a program.
+ 4) Polymorphism (Polimorfismo).- It is the ability of one function to perform in different ways. 
+    It can be applied in two simple ways:
+    4.1 Overloading - A class with multiple methods with the same names but a different set of parameters, 
+        or different return types, or different access modifiers.
+    4.2 Override: This is only possible if a subclass (or) sister class has the same method as the parent class, 
+        has the same parameter list, same return type, and only has different behavior.
+
+ Object.-
+ Class.- A class is like a template from which new objects are created.
+
+ https://www.sanfoundry.com/object-oriented-programming-questions-answers-constructors-overloading/
+*/
