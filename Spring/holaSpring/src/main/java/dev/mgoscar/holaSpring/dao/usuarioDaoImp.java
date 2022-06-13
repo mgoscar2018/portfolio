f231eff6eec8a82112cfb25196c3da463db41eb1
mgoscar <mgoscar@gmail.com>
2022-06-13 12:51:43 -0500

    Warning fixes

--- Spring/holaSpring/src/main/java/dev/mgoscar/holaSpring/dao/usuarioDaoImp.java
+++ Spring/holaSpring/src/main/java/dev/mgoscar/holaSpring/dao/usuarioDaoImp.java
@@ -2,27 +2,27 @@ package dev.mgoscar.holaSpring.dao;
 
 import dev.mgoscar.holaSpring.models.Usuario;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import java.util.List;
 
 @Repository
-@Transactional
+//@Transactional //Se comenta ya que en este lugar por el momento no le veo la utilidad 
 public class usuarioDaoImp implements usuarioDao{
 
     @PersistenceContext
-    private EntityManager entityManager; //permite realizar la conexión con la BD
+    private EntityManager em; //permite realizar la conexión con la BD
 
-    @Override
-    @Transactional
+    @Override    
     public List<Usuario> getUsuarios() {
         String consulta = "FROM Usuario"; // "Usuario" es el nombre de la clase, no de la tabla
-        
-        List<Usuario> resultado = entityManager.createQuery(consulta).getResultList();         
+        /*
+        List<Usuario> resultado = em.createQuery(consulta,Usuario.class).getResultList();
         return resultado;
-         
-        //return entityManager.createQuery(consulta).getResultList();
+         */
+        return em.createQuery(consulta,Usuario.class).getResultList();        
     }
 }
